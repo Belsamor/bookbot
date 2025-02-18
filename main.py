e443ad5101570de004c7b4b74f07c2db3342e69b
Belsamor <larspomsel@gmx.de>
2025-02-18 19:35:47 +0100

    added char counting

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+    count_character(text)
 
-
-def count_words(text):
+def get_num_words(text):
     words = text.split()
     return len(words)
 
 
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
-if __name__ == '__main__':
-    print(file_contents)
-    print(count_words(file_contents))
+def count_character(text):
+    char_dict = dict()
+    for character in text:
+        if character.lower() in char_dict:
+            char_dict [character.lower()] += 1
+        else:
+            char_dict [character.lower()] = 1
+    return print(char_dict)
+
+main()
