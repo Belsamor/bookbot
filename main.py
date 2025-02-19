622f45ba6739a51b61be892b53114e77be278c5b
Belsamor <larspomsel@gmx.de>
2025-02-19 19:44:43 +0100

    created ordered character list

--- main.py
+++ main.py
@@ -17,10 +17,21 @@ def get_book_text(path):
 def count_character(text):
     char_dict = dict()
     for character in text:
-        if character.lower() in char_dict:
-            char_dict [character.lower()] += 1
-        else:
-            char_dict [character.lower()] = 1
-    return print(char_dict)
+        if character.isalpha():
+            if character.lower() in char_dict:
+                char_dict [character.lower()] += 1
+            else:
+                char_dict [character.lower()] = 1
 
+    char_list = []
+    for e in char_dict:
+        char_info = {'char': e, 'num': char_dict[e]}
+        char_list.append(char_info)
+    char_list.sort(reverse=True, key=sort_on)
+
+    for item in char_list:
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+def sort_on(dict):
+    return dict["num"]
 main()
